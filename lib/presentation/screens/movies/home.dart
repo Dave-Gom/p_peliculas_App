15f3e50b337e58d07d9eeaf9fe1e71dd834b19cd
Dave Gomez <davegomez426@gmail.com>
2024-04-14 22:15:56 -0400

    inifinite scroll + carga de otras pelicular

--- lib/presentation/screens/movies/home.dart
+++ lib/presentation/screens/movies/home.dart
@@ -29,23 +29,55 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   void initState() {
     super.initState();
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(pupularMoviesProviders.notifier).loadNextPage();
+    ref.read(topRatedMoviesProviders.notifier).loadNextPage();
+    ref.read(upcomingMoviesProviders.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideShowProvider);
+    final popularMovies = ref.watch(pupularMoviesProviders);
+    final upcomingMovies = ref.watch(upcomingMoviesProviders);
+    final topRatedMovies = ref.watch(topRatedMoviesProviders);
 
-    return Column(
-      children: [
-        const CustomAppbar(),
-        MovieSlidershow(movies: slideShowMovies),
-        MovieHorizontalListView(
-          movies: nowPlayingMovies,
-          title: "En cines",
-          subtitle: "Lunes 20",
-        )
-      ],
-    );
+    return CustomScrollView(slivers: [
+      SliverAppBar(
+        floating: true,
+        flexibleSpace: FlexibleSpaceBar(title: CustomAppbar()),
+      ),
+      SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+        return Column(
+          children: [
+            MovieSlidershow(movies: slideShowMovies),
+            MovieHorizontalListView(
+                movies: nowPlayingMovies,
+                title: "En cines",
+                subtitle: "Lunes 20",
+                loadNextPage:
+                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage),
+            MovieHorizontalListView(
+                movies: upcomingMovies,
+                title: "Proximamente",
+                subtitle: "Este mes",
+                loadNextPage:
+                    ref.read(upcomingMoviesProviders.notifier).loadNextPage),
+            MovieHorizontalListView(
+                movies: popularMovies,
+                title: "Populares",
+                loadNextPage:
+                    ref.read(pupularMoviesProviders.notifier).loadNextPage),
+            MovieHorizontalListView(
+                movies: topRatedMovies,
+                title: "Mejor calificadas",
+                subtitle: "Lunes 20",
+                loadNextPage:
+                    ref.read(topRatedMoviesProviders.notifier).loadNextPage)
+          ],
+        );
+      }, childCount: 1))
+    ]);
   }
 }
